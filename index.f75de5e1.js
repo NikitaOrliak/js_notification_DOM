85b4b84e1882f46b3eb9067db6d6841aabe05539
Nikita Orliak <nikitonorl@gmail.com>
2023-11-26 18:50:34 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const pageBody = document.querySelector("body");
+    const message = document.createElement("div");
+    message.className = `notification ${type}`;
+    message.style.top = `${posTop}px`;
+    message.style.right = `${posRight}px`;
+    const messageTitle = document.createElement("h2");
+    messageTitle.className = "title";
+    messageTitle.textContent = title;
+    const messageDescription = document.createElement("p");
+    messageDescription.textContent = description;
+    message.append(messageTitle);
+    message.append(messageDescription);
+    pageBody.append(message);
+    setTimeout(()=>message.remove(), 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
